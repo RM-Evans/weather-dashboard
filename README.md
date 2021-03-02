2d92962a0b4a2628cffc8372f1b9aa873dfb8320
RM-Evans <evansrichardmichael@gmail.com>
2021-03-01 21:10:29 -0700

    completed: added cards-next 5 days weather

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # weather-dashboard
+
+
+1. [General Info](#general-info)
+2. [Deployed Application](#deployed-application)
+3. [Screenshot](#screenshot)
+4. [Technologies](#technologies)
+5. [SETUP](#setup)
+
+##General Info
+***
+A weather dashboard that allows you to search by city for the current (and next 5 days) temperature, humidity, wind speed, and UV index.
+
+## Deployed Application
+***
+
+https://rm-evans.github.io/weather-dashboard/
+
+
+##Screenshot
+***
+[![wdscreenshot.png](https://i.postimg.cc/tRWnYqbD/wdscreenshot.png)](https://postimg.cc/w3xjrd7s)
+
+## Technologies
+***
+*HTML
+*CSS
+*JavaScript
+*api.openweathermap.org/data/2.5/
+    -one call
+    -current weather
+
+##SETUP
+***
+Use own API key in place of {APIKEY} to make functional
+
+https://home.openweathermap.org/
--- app.js
+++ app.js
@@ -11,7 +11,12 @@ async function callWeather(searchTerm) {
                 .then(response => response.json())
                 console.log(onecall)
 //passing weather, onecall so I can pick through two api endpoints
+
+//TODO handle errors
+
 callWeatherHTML(weather, onecall)
+
+generateDayCard(onecall)
     // fetch(
     //     'https://api.openweathermap.org/data/2.5/weather?q=' +
     //     searchTerm +
@@ -72,6 +77,7 @@ function callWeatherHTML(weather, onecall) {
     currentCityImg.src = `http://openweathermap.org/img/wn/${onecall.current.weather[0].icon}.png`
 
 
+        //color code uv index
     
         if (onecall.current.uvi > 6) {
             uvIndex.className = 'highUVI'
@@ -86,5 +92,59 @@ function callWeatherHTML(weather, onecall) {
         }
     
 }
+
+function generateDayCard(onecall) {
+
+    let weekWeather = document.querySelector('.weekWeather')
+
+    weekWeather.innerHTML = ''
+    
+    //onecall is response
+    let days = onecall.daily
+    console.log(days) 
+
+//     <div class="card">
+//     <div class="cardBody">
+//         <h2>date</h2>
+//         <img src="" alt="">
+//         <p>tempF</p>
+//         <p>humidity%</p>
+//     </div>
+// </div>
+
+
+    //for five days - not including [5] - till [4]
+    for (let i = 0; i < 5; i++){
+        let day = days[i + 1]
+        //create card
+        let card = document.createElement('div')
+        card.classList.add('card')
+        //create body
+        let cardBody = document.createElement('div')
+        cardBody.classList.add('cardBody')
+        // h2 date new Date(Date.now()).toDateString()
+        let dayOfWeek = document.createElement('h3')
+        dayOfWeek.innerText = (new Date(day.dt * 1000).toDateString())
+        // img icon
+        let weekImg = document.createElement('img')
+        weekImg.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`
+        // p   day.temp.day
+        let weekTemp = document.createElement('p')
+        weekTemp.textContent = `Temp: ${day.temp.day}°F`
+        // p   day.humidity
+        let weekHumidity = document.createElement('p')
+        weekHumidity.textContent = `Humidity: ${day.humidity}%`
+
+        cardBody.appendChild(dayOfWeek)
+        cardBody.appendChild(weekImg)
+        cardBody.appendChild(weekTemp)
+        cardBody.appendChild(weekHumidity)
+        card.appendChild(cardBody)
+        weekWeather.appendChild(card)
+
+    }
+}
+
+
 
 
